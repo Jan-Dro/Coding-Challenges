07879d950d7359e37c7bd0f7f2c88ba4e71e2579
jandro <jandrocodes@icloud.com>
2024-01-21 16:30:40 -0600

    Maximum Subarray

--- main.py
+++ main.py
@@ -1,11 +1,33 @@
-def containsDuplicate(nums):
-    numDict = {}
+# def productExceptSelf(nums):
+#     res = [1] * (len(nums))
+#     # print(res)
 
-    for num in nums:
-        if num in numDict:
-            return True
-        else:
-            numDict[num] = num
-    return False
+#     prefix = 1
+#     for i in range(len(nums)):
+#         res[i] = prefix
+#         print(res[i], res)
+#         prefix *= nums[i]
+#         print('--------')
+#         # print(prefix)
+#     postfix = 1
+#     for i in range(len(nums) -1, -1, -1):
+#         res[i] *= postfix
+#         postfix *= nums[i]
+#     return res    
 
-print(containsDuplicate([1,2,3,1]))
+# print(productExceptSelf([1,2,3,4]))
+
+
+def maxSubArray(nums):
+    maxSub = nums[0]
+    currentSum = 0
+
+    for number in nums:
+        if currentSum < 0:
+            currentSum = 0
+        currentSum += number
+        maxSub = max(maxSub, currentSum)
+    return maxSub
+
+
+print(maxSubArray([5,4,-1,7,8]))
