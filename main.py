9b264aaf984a039ffe3ce25d37d955fb72599b38
jandro <jandrocodes@icloud.com>
2024-01-22 15:58:48 -0600

    missing number

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 #Binary
 
-def countBits(n):
-    res = [bin(i).count('1') for i in range(n + 1)]
-    return res
+# def countBits(n):
+#     res = [bin(i).count('1') for i in range(n + 1)]
+#     return res
 
-print(countBits(5)) 
+# print(countBits(5)) 
+
+
+
+def missingNum(nums):
+    nums.sort()
+    
+    for i, number in enumerate(nums):
+        if number != i:
+            return i
+    return len(nums)
+        
+
+print(missingNum([0,1]))
