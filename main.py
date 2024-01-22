49d8a4b5d98134d771446b4311e95042962c4f24
jandro <jandrocodes@icloud.com>
2024-01-22 15:45:49 -0600

    count bits

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
 #Binary
 
-def hammingWeight(n):
-    count = 0
-    while n:
-        count += n & 1
-        n >>= 1
-    return count
+def countBits(n):
+    res = [bin(i).count('1') for i in range(n + 1)]
+    return res
 
-print(hammingWeight(11111111111111111111111111111101)) 
+print(countBits(5)) 
