e81ae95fef38aecefd34811511ade5b9bcfa60c2
jandro <jandrocodes@icloud.com>
2024-01-22 12:55:20 -0600

    Search In Rotated Sorted Array

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
-def findMin(nums):
+def search(nums, target):
     start = 0
-    end = len(nums) - 1
-    minimumNum = nums[0]
-
+    end = len(nums) -1
     while start <= end:
-        minimumNum = min(minimumNum ,nums[start], nums[end])
-        start += 1
-        end -= 1
-    return minimumNum
 
-print(findMin([11,13,15,17]))
+        if nums[start] == target:
+            return start
+        elif nums[end] == target:
+            return end
+        else:
+            start += 1
+            end -= 1
+    return -1
 
 
+print(search([4,5,6,7,0,1,2], 3))
