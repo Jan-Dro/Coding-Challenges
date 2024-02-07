0f8ca9c34612eb0f944d74b74e308a9e91229744
jandro <jandrocodes@icloud.com>
2024-02-06 18:43:39 -0600

    Number of Students Unable to Eat Lunch

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
-def plusOne(digits):
+def countStudents(students, sandwiches):
+    count = 0
+    while students and sandwiches:
+        if students[0] == sandwiches[0]:
+            students.pop(0)
+            sandwiches.pop(0)
+            count = 0  
+        else:
+            student = students.pop(0)
+            students.append(student)
+            count += 1
 
-    numStorage = ''
+        if count >= len(students):
+            break
 
-    for number in digits:
-        numStorage += str(number)
+    return len(students)
 
-    numStorage = str(int(numStorage) + 1)
-    answer = []
-
-    for number in numStorage:
-        answer.append(int(number))
-
-    return answer
-
-
-print(plusOne([9]))
+print(countStudents([1,1,1,0,0,1], [1,0,0,0,1,1]))
+print(countStudents([1,1,0,0], [0,1,0,1]))
