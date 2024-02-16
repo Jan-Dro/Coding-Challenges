c5ac880656c86a25b13beb2ea81a4f0780efc855
jandro <jandrocodes@icloud.com>
2024-02-15 18:10:23 -0600

    merge intervals

--- main.py
+++ main.py
@@ -26,4 +26,20 @@ def addTwoNumbers(l1, l2):
         current.next = ListNode(carry)
     
     return dummy.next
+def mergeIntervels(nums):
+    if not nums:
+        return []
+    
+    nums.sort(key=lambda x: x[0])
+
+    index = 0
+
+    while index < len(nums) - 1:
+        if nums[index][1] >= nums[index + 1][0]:
+            nums[index][1] = max(nums[index][1], nums[index + 1][1])
+            del nums[index + 1]
+        else:
+            index+= 1
+
+    return nums
 
