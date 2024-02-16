49eceb222f92e1f88fc19094e388882fe676f1da
jandro <jandrocodes@icloud.com>
2024-02-15 18:13:52 -0600

    merge intervals

--- main.py
+++ main.py
@@ -1,31 +1,3 @@
-class ListNode(object):
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
-
-def addTwoNumbers(l1, l2):
-    dummy = ListNode(0)
-    current = dummy
-    carry = 0
-
-    while l1 or l2:
-        val1 = l1.next if l1 else 0
-        val2 = l2.next if l2 else 0
-        sum = val1 + val2 + carry
-
-        carry == sum // 10
-        current.next = ListNode(sum % 10)
-        current = current.next
-
-        if l1:
-            l1 = l1.next
-        if l2:
-            l2 = l2.next
-
-    if carry > 0:
-        current.next = ListNode(carry)
-    
-    return dummy.next
 def mergeIntervels(nums):
     if not nums:
         return []
@@ -43,3 +15,5 @@ def mergeIntervels(nums):
 
     return nums
 
+
+print(mergeIntervels([[1,3], [2,6], [8,10], [15,18]]))
